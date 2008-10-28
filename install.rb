0f15cbf1d24dd8398ef6b3e1657e2ea1e1b947bd
Felipe Talavera <felipe.talavera@gmail.com>
2008-10-28 16:19:11 +0100

    create a db/migrate dir if it's not created yet during the install.rb execution

--- install.rb
+++ install.rb
@@ -3,7 +3,11 @@ require 'fileutils'
 stats_config = File.dirname(__FILE__) + '/../../../config/stats_for_all.yml'
 FileUtils.cp File.dirname(__FILE__) + '/templates/stats_for_all.yml', stats_config unless File.exist?(stats_config)
 
+migrate_dir=File.dirname(__FILE__) + "/../../../db/migrate/"
+Dir.mkdir( migrate_dir) unless File.exist?(migrate_dir)
+
 migration_config = File.dirname(__FILE__) + "/../../../db/migrate/#{Time.now.strftime("%Y%m%d%H%M%S")}_create_stats_for_all.rb"
 FileUtils.cp File.dirname(__FILE__) + '/templates/create_stats_for_all.rb', migration_config # unless File.exist?(migration_config)
 
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+
