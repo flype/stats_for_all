e43e3004c903c679c1b580f4902784f1f0b0959d
Felipe Talavera <felipe.talavera@gmail.com>
2008-10-29 11:40:50 +0100

    migration index modified

--- templates/create_stats_for_all.rb
+++ templates/create_stats_for_all.rb
@@ -15,11 +15,11 @@ class CreateStatsForAll < ActiveRecord::Migration
   end
 
   def self.down
-    drop_table :stats
+    # remove_index :stats,  :name =>"idx_stats_day"
+    # remove_index :stats,  :name =>"idx_stats_month"
+    # remove_index :stats,  :name =>"idx_stats_year"
+    # remove_index :stats,  :name =>"idx_stats_stat_type"
     
-    remove_index :stats,  :name =>"idx_stats_day"
-    remove_index :stats,  :name =>"idx_stats_month"
-    remove_index :stats,  :name =>"idx_stats_year"
-    remove_index :stats,  :name =>"idx_stats_stat_type"
+    drop_table :stats
   end
 end
